35053ea1fe7d133a4d6e2a97fe530721702d5db2
LONDON\coralb <coralb@london.edu>
2023-02-02 15:10:34 +0000

    Tested/added lambda AreNamesSame

--- Iteration Exercises/Lambda.cs
+++ Iteration Exercises/Lambda.cs
@@ -9,6 +9,8 @@ namespace Iteration_Exercises
         public static Func<string, string, string> Title = (honorific, name) => throw new NotImplementedException();
         public static Func<string, bool> StartsWithA = name => name.StartsWith("A");
         public static Func<string, int> NameLength = name => name.Length;
+        public static Func<string, string, bool> AreNamesSame = (name1, name2) => name1 == name2;
+
 
 
         /* Test-drive more lambdas which will:
@@ -22,11 +24,33 @@ namespace Iteration_Exercises
          * least one reason on each side!
          */
 
+        /*
+         
+         Pros of using 'name' as a string:
+
+            - String is a commonly used data type for representing names and is easily understandable.
+            - String is an efficient data type for manipulation and processing purposes (stored in memory).
+
+
+        Cons of using 'name' as a string:
+
+            - Strings are immutable, this can lead to increased memory usage when manipulating strings.
+            - Using string for representing names might not be flexible enough to accommodate 
+                other data types or formats that may be needed for representing names.
+            - Processing large strings could affect the performance of the application.
+
+         */
+
         /* ===== PART TWO ===== */
 
         public static IEnumerable<string> GreetAll(IEnumerable<string> names)
         {
             return names.Select(Greet);
+        }
+
+        public static IEnumerable<string> GetNamesStartingWithA(IEnumerable<string> names)
+        {
+            return names.Where(StartsWithA);
         }
 
         /* using the lambdas you created above, test-drive methods to:
--- IterationExercises.Test/LambdaTests.cs
+++ IterationExercises.Test/LambdaTests.cs
@@ -10,20 +10,11 @@ public class LambdaTests
         Assert.That(Lambda.Greet("Alex"), Is.EqualTo("Hi Alex"));
     }
 
-
     [Test]
     [Ignore("Not implemented yet - please work through looping material first")]
     public void LambdaGivesTitle()
     {
         Assert.That(Lambda.Title("Mx", "Addison"), Is.EqualTo("Mx Addison"));
-    }
-
-    [Test]
-    public void GreetsAll()
-    {
-        var names = new List<string> { "Alice", "Bob", "Carol" };
-        IEnumerable<string> expected = new List<string> { "Hi Alice", "Hi Bob", "Hi Carol" };
-        Assert.That(Lambda.GreetAll(names), Is.EquivalentTo(expected));
     }
 
     [Test]
@@ -37,4 +28,22 @@ public class LambdaTests
     {
         Assert.That(Lambda.NameLength("Alex"), Is.EqualTo(4));
     }
+
+    [Test]
+    public void LambdaAreNamesSameTest()
+    {
+        string name1 = "Alice";
+        string name2 = "Bob";
+        bool expected = false;
+        Assert.That(Lambda.AreNamesSame(name1, name2), Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void GreetsAll()
+    {
+        var names = new List<string> { "Alice", "Bob", "Carol" };
+        IEnumerable<string> expected = new List<string> { "Hi Alice", "Hi Bob", "Hi Carol" };
+        Assert.That(Lambda.GreetAll(names), Is.EquivalentTo(expected));
+    }
+
 }
